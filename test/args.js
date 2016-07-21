4f8d04b100e23f9d7f542f7eb88b69f4297e202f
matutter <mcutter.svc@gmail.com>
2016-07-21 00:45:28 +0000

    removed flawed test

--- test/args.js
+++
@@ -1,9 +0,0 @@
-var ato = require('..')
-var assert = require('chai').assert
-
-describe('ato on actual argv', ()=> {
-  it('should produce a flat object', ()=> {
-    var params = ato.parse(process.argv, {})
-    assert.deepEqual(params, {})
-  })
-})
